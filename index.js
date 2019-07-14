79ccd169a26b4706872874818585dc9b5143574b
Bowen Yin <bowenvictoryin@gmail.com>
2019-07-13 23:42:03 -0700

    Test socket.io stuff

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+require("dotenv").config();
 const app=require("express")();
-const http=require("http").Server(app);
-const io=require("socket.io")(http);
+const socket=require("socket.io");
 
 app.get("/", (req, res)=>{
   
 });
-app.listen(5000, ()=>{
+const server=app.listen(process.env.PORT, ()=>{
   console.log("server is running on port");
+});
+const io=socket(server, {pingTimeout: 30000}); // consider increasing pingTimeout
+io.on("connection", socket=>{
+  console.log("connected "+socket.id);
+  io.emit("test", "heyy");
+  setTimeout(()=>{
+    io.emit("test", "10 seconds later");
+  }, 10000);
+  socket.on("disconnect", ()=>{
+    console.log("disconnected "+socket.id);
+  });
 });
