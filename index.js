ac453420fe354c52974f7871bbc93b4016bde22e
Bowen Yin <bowenvictoryin@gmail.com>
2019-07-30 13:48:46 -0700

    Add route for Google Assistant fulfillment

--- index.js
+++ index.js
@@ -7,31 +7,65 @@ const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(process.env.DB_HOST).then(client => {
   const db = client.db(process.env.DB_NAME);
   
-}).catch(err => {
-  console.log(err);
-  throw err;
-})
+  app.use(express.json()); // use new built-in Express middleware to parse JSON bodies
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send(process.env);
-});
-app.post("/assistant", (req, res) => {
-  res.send(req.body);
-});
-
-const server = app.listen(5000, () => {
-  console.log("server is running on port");
-});
-const io = socket(server, {pingTimeout: 30000}); // consider increasing pingTimeout
-io.on("connection", socket => {
-  console.log("connected "+socket.id);
-  io.emit("test", "heyy");
-  setTimeout(() => {
-    io.emit("test", "10 seconds later");
-  }, 10000);
-  socket.on("disconnect", () => {
-    console.log("disconnected "+socket.id);
+  app.get("/", (req, res) => {
+    res.send(process.env);
   });
+  /** Responds with the bell schedule when a request from Actions on Google/Google Assistant is received. */
+  app.post("/assistant", async (req, res) => {
+    /** @type {Date} */
+    let date = new Date(req.body.queryResult.parameters.date);
+    const now = new Date();
+    let formattedDate = date.toLocaleDateString(undefined, {
+      timeZone: "UTC",
+      weekday: "short",
+      month: "long",
+      day: "numeric",
+      year: date.getUTCFullYear() == now.getFullYear() ? undefined : "numeric",
+    });
+    let schedule = await db.collection("schedules").findOne({date});
+    let str = "";
+    if (schedule) {
+      str += formattedDate+(now-date>now.getTimezoneOffset()*60*1000 ? " is " : " was ");
+      if (schedule.variant) {
+        str += startsWithVowel(schedule.variant) ? "an " : "a ";
+        str += schedule.variant+' "'+schedule.code+'"';
+      } else {
+        str += startsWithVowel(schedule.code) ? "an " : "a ";
+        str += '"'+schedule.code+'"';
+      }
+      str += " day."
+    } else {
+      str += "Sorry, I couldn't find a schedule for "+formattedDate+".";
+    }
+    res.send({
+      fulfillment_text: str,
+    });
+  });
+  const server = app.listen(process.env.PORT, () => {
+    console.log("server is running on port");
+  });
+  
+  const io = socket(server, {pingTimeout: 30000}); // consider increasing pingTimeout
+  io.on("connection", socket => {
+    console.log("connected "+socket.id);
+    io.emit("test", "heyy");
+    setTimeout(() => {
+      io.emit("test", "10 seconds later");
+    }, 10000);
+    socket.on("disconnect", () => {
+      console.log("disconnected "+socket.id);
+    });
+  });
+}).catch(err => {
+  throw err;
 });
+
+/**
+ * 
+ * @param {string} str  the string to test
+ */
+function startsWithVowel(str) {
+  return ["a", "e", "i", "o", "u"].includes(str.toLowerCase().substring(0, 1));
+}
